ebf0f7980f5bf1818ced956de65971cafc945f04
maciekzdyb <maciekzdyb@gmail.com>
2023-06-16 11:24:09 +0200

    first commit

---
+++ src/main/java/com/example/readTxt/ReadTxtApplication.java
@@ -0,0 +1,13 @@
+package com.example.readTxt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadTxtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadTxtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/readTxt/ReadTxtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.readTxt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadTxtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
